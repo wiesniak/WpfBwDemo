e3d5a6a1492e38ede69be5cc1a97a8d192bc1097
wiesniak <wiesniak_xl@wp.pl>
2023-02-05 21:47:15 +0100

    Initial commit - working demo

---
+++ WpfBwDemo/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBwDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BackgroundWorker worker;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+        }
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            // I could use worker from the class but IMO it would be not nice to reach from thread to outside
+            var ownerWorker = (BackgroundWorker)sender!;
+            int loopsCounter = 0;
+            while (!ownerWorker.CancellationPending)
+            {
+                loopsCounter++;
+                Dispatcher.Invoke(UpdateCounter);
+                ownerWorker.ReportProgress(0, loopsCounter);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void Worker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            Dispatcher.Invoke(() => LogMessage("Finished"));
+        }
+
+        private void Worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            var loopsCounter = (int)e.UserState!;
+            Dispatcher.Invoke(() => LogMessage($"I did {loopsCounter} already! ＼(^o^)／"));
+        }
+
+        public void UpdateCounter()
+        {
+            if (!int.TryParse(CounterTextBox.Text, out var value))
+            {
+                value = 0;
+            }
+
+            value++;
+            CounterTextBox.Text = value.ToString();
+        }
+
+        private void MagicButton_Click(object sender, RoutedEventArgs e)
+        {
+            // If there is cancellation pending and the worker is still running, we don't want to call CancelAsync multiple times
+            if (worker.IsBusy && !worker.CancellationPending)
+            {
+                worker.CancelAsync();
+                LogMessage("Cancelling...");
+            }
+            else if (!worker.IsBusy)
+            {
+                worker.RunWorkerAsync();
+                LogMessage("Running...");
+            }
+            else
+            {
+                LogMessage("Skipping...");
+            }
+        }
+
+        private void LogMessage(string message)
+        {
+            var newLine = LogTextBox.Text.Length == 0 ? "" : Environment.NewLine;
+            LogTextBox.Text += $"{newLine}[{DateTime.Now}] {message}";
+            LogTextBox.ScrollToEnd();
+        }
+    }
+}
